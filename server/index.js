cfd7a0156fc0f6556bce78c478eb3dc84ba36d54
Derek Scott <dered806@gmail.com>
2020-09-15 08:38:53 -0600

    server set up, connection to db started

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require ('dotenv').config();
 const express = require ('express');
 const massive = require ('massive');
+const cors = require ('cors');
 const ctrl = require('./controller')
 
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
@@ -8,12 +9,23 @@ const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {
+        maxAge: 60000,
+    }
+
+}));
 
 
 
 
 massive(CONNECTION_STRING).then(db => {
-    app.set('db', db)
+    app.set('db',db)
 
-    app.listen(SERVER_PORT , () => console.log (`live on {$SERVER_PORT}`))
-});
+ app.listen(SERVER_PORT, () => console.log(`live on ${SERVER_PORT} 🚀 `))
+
+})
