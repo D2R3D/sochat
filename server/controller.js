20d9c970c115c450340891e06949e120e0d21938
Derek Scott <dered806@gmail.com>
2020-09-15 10:46:56 -0600

    server connected and live w/ db user file

---
+++ server/controller.js
@@ -0,0 +1,6 @@
+// const bcrypt = require('bcryptjs')
+
+// const register = async (req, res) =>{
+//     const db = req.app.get('db')
+//     const {username, password, profile_pic} = req.body
+// }
--- server/index.js
+++ server/index.js
@@ -1,31 +1,29 @@
-require ('dotenv').config();
-const express = require ('express');
-const massive = require ('massive');
-const cors = require ('cors');
-const ctrl = require('./controller')
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const session = require('express-session')
+const cors = require('cors')
 
-const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env
 
-const app = express();
+const app = (express()) 
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 app.use(session({
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     secret: SESSION_SECRET,
     cookie: {
-        maxAge: 60000,
+        maxAge: 60000 *10
     }
-
-}));
-
-
-
+}))
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db',db)
-
- app.listen(SERVER_PORT, () => console.log(`live on ${SERVER_PORT} 🚀 `))
-
+}).catch(error => {
+    console.log(error)
 })
+
+app.listen(SERVER_PORT, () => console.log(`living on ${SERVER_PORT}`))
+
